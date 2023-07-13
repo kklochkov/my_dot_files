8a965769772a780e7c334e9857ef1899a2eec137
Kirill Klochkov <kyrylo.klochkov@fernride.com>
2023-07-13 19:37:19 +0200

    wezterm update

--- common/.wezterm.lua
+++ common/.wezterm.lua
@@ -1,11 +1,27 @@
 local wezterm = require 'wezterm';
+local mux = wezterm.mux
 
 --wezterm.on("update-right-status", function(window, pane)
 --  local success, status, stderr = wezterm.run_child_process{"python3", "-u", "/home/kklochkov/develop/my_dot_files/common/my_i3_status.py", "-m", "shell", "-d", "/dev/mapper/vgubuntu-root"}
 --  window:set_right_status(status);
 --end);
 
-return {
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():toggle_fullscreen()
+end)
+
+wezterm.on('gui-attached', function(domain)
+  -- maximize all displayed windows on startup
+  local workspace = mux.get_active_workspace()
+  for _, window in ipairs(mux.all_windows()) do
+    if window:get_workspace() == workspace then
+      window:gui_window():toggle_fullscreen()
+    end
+  end
+end)
+
+local config = {
   -- https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/DroidSansMono
   font = wezterm.font_with_fallback {
     'DroidSansM Nerd Font Mono',
@@ -57,3 +73,5 @@ return {
   },
   pane_focus_follows_mouse = true,
 }
+
+return config
